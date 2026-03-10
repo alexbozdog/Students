90489ca2ebe0dafcf50eda6112baa514c3e4a0cd
alexbozdog <alexandrustefan.bozdog@ulbsibiu.ro>
2026-03-10 09:26:17 +0200

    rezolvare lab2

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,22 +1,54 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static boolean existaStudent(List<Student> lista, Student s) {
+        for (Student st : lista) {
+            if (st.getPrenume().equals(s.getPrenume()) &&
+                    st.getNume().equals(s.getNume()) &&
+                    st.getFormatieDeStudiu().equals(s.getFormatieDeStudiu())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main() {
-        Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
-        Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
-        Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
-        Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
-        Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
 
+        List<Student> listaStudenti = new ArrayList<>();
+
+        listaStudenti.add(new Student(112, "Ioan", "Popa", "TI21/1"));
+        listaStudenti.add(new Student(112, "Maria", "Oprea", "TI21/1"));
+        listaStudenti.add(new Student(120, "Alis", "Popa", "TI21/2"));
+        listaStudenti.add(new Student(122, "Mihai", "Vecerdea", "TI22/1"));
+        listaStudenti.add(new Student(122, "Eugen", "Uritescu", "TI22/2"));
+
+     /*
         System.out.println(String.format("%15s %20s %15s",
                 "numar matricol", "prenume nume", "formatieDeStudiu"));
+
 
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
         System.out.println(s4);
         System.out.println(s5);
+       */
+
+        // a)
+        for(Student s : listaStudenti){
+            System.out.println(s);
+        }
+        // b)
+        Student s1 = new Student(120,"Alis","Popa","TI21/2");
+        System.out.println(existaStudent(listaStudenti, s1));
+
+        //c)
+        Student s2=new Student(112, "Maria", "Popa", "TI21/1");
+        System.out.println(existaStudent(listaStudenti, s2));
+
     }
 
 
